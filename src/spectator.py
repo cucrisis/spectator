085670fccbd7323247139911bba0d2219ea4afd1
cucrisis <cucrisis@Insula.fios-router.home>
2017-08-06 22:56:46 -0400

    common regex update

--- src/spectator.py
+++ src/spectator.py
@@ -7,7 +7,7 @@ from console import parser
 # Setup workspace
 SPEC_HCS_ACCESS_TOKEN = os.getenv('SPECTATOR_HCS_TOKEN', '')
 SPEC_OUTPUT_FILE_PATH = os.getenv('SPECTATOR_LOG', '../config/spectator.log')
-SPEC_REGEX_FILE_PATH = os.getenv('SPECTATOR_CONFIG', '../config/spectator.common.json')
+SPEC_REGEX_FILE_PATH = os.getenv('SPECTATOR_CONFIG', '../config/spectator.common.regex.json')
 
 # Setup logger
 logging.basicConfig(filename=SPEC_OUTPUT_FILE_PATH, level=logging.INFO)
